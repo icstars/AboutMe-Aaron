f42827e5f37fd7727acff8c2e840651f8e52a612
Aaron L <asanders@icstars.org>
2023-06-26 14:30:35 -0500

    yur 6/26

---
+++ forloops.js
@@ -0,0 +1,31 @@
+// FOR LOOP
+// initializer
+// condition
+
+const arr = [1,2,3]
+         // [0,1,2]
+console.log(arr[2])
+
+for (let initializer = 0; initializer < arr.length; initializer++) {
+    console.log(arr[initializer])
+   
+    // FUNCTIONS
+    function iterateThroughArray(arr){
+        for (let initializer = 0; initializer < arr.length; initializer++) {
+            console.log(arr[initializer])
+        }
+    }
+
+    iterateThroughArray(array3)
+
+    function sum1(num1, num2) {
+        console.log(num1 + num2)
+    }
+    
+
+
+
+
+
+
+}
